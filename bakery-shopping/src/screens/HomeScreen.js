d8c36c98f6197d852e0eab2cdb2350f886e1ef69
Sandra Coburn <sandra_coburn@hotmail.com>
2021-01-17 20:48:18 -0800

    Added styling to new pages

--- bakery-shopping/src/screens/HomeScreen.js
+++ bakery-shopping/src/screens/HomeScreen.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Filter from '../components/Filter';
-import Products from '../components/Products';
-import Cart from '../components/Cart';
+import Hero from '../components/Hero';
 
 const HomeScreen = () => {
   return (
-    <div>
-      <div className="content">
-        <div className="main">
-          <Filter />
-          <Products />
-        </div>
-        <div className="sidebar">
-          <Cart />
-        </div>
-      </div>
+    <div className="home">
+      <Hero />
     </div>
   );
 };
